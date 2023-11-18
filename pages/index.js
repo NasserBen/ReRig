148089d4287b6cad3b808b131c5a824db387a9ef
Karl <karldrummer12@gmail.com>
2023-11-18 15:01:15 -0800

    Adding filterParts.jsx

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/navbar";
+import FilterParts from "../components/filterParts";  
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <FilterParts />
     </div>
   );
 }
