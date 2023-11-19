1f3e6ded6d88cbcaae5d5ce5fecf89a4e91fdc84
Nasser Ben <nasserbenyed@gmail.com>
2023-11-18 16:00:31 -0800

    add components to Hone

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Navbar from "../components/navbar";
-import FilterParts from "../components/filterParts";  
+import FilterParts from "../components/filterParts";
 import Footer from "../components/footer";
+import Post from "../components/post";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <FilterParts />
-      <Footer/>
+      <Post />
+      <Footer />
     </div>
   );
 }
